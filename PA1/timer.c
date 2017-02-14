0d411c17ef87414eaedb8b4132b222dca038caa9
BrianJConway <brianc@nevada.unr.edu>
2017-02-14 07:52:55 -0800

    added timing

--- PA1/timer.c
+++ PA1/timer.c
@@ -8,6 +8,8 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
 
 int main (int argc, char *argv[])
 {
@@ -15,6 +17,8 @@ int numtasks, rank, dest, source, rc, count, tag=1;
 int inmsg, outmsg=1;
 MPI_Status Stat;
 int index;
+struct 	timeval tv1, tv2;
+double totalTime = 0;
 
 MPI_Init(&argc,&argv);
 MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
@@ -37,11 +41,13 @@ if (rank == 0)
   dest = 1;
   source = 1;
   
-  for( index = 0; index < 100; index++ )
+  for( index = 0; index < 1000; index++ )
   {
+    tv1 = MPI_Wtime();
     rc = MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
     rc = MPI_Recv(&inmsg, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &Stat);
-    printf("%d \n",index);
+    tv2 = MPI_Wtime();
+    totalTime += tv2 - tv1;
   }
 }
 
@@ -50,7 +56,7 @@ else if (rank == 1)
   dest = 0;
   source = 0;
 
-  for( index = 0; index < 100; index++ )
+  for( index = 0; index < 1000; index++ )
   {
     rc = MPI_Recv(&inmsg, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &Stat);
     rc = MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
@@ -58,10 +64,8 @@ else if (rank == 1)
 
 }
 
-if (rank < 2) {
-  rc = MPI_Get_count(&Stat, MPI_INT, &count);
-  printf("Task %d: Received %d int(s) from task %d with tag %d \n",
-         rank, count, Stat.MPI_SOURCE, Stat.MPI_TAG);
+if (rank == 0 ) {
+  printf("%d \n", totalTime);
   }
 
  MPI_Finalize();
