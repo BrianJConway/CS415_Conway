cce6baa420d11e58229487a85f0a4c416d0cce3f
BrianJConway <brianc@nevada.unr.edu>
2017-02-20 14:38:32 -0800

    Added timer

---
+++ PA1/src/Timer.cpp
@@ -0,0 +1,101 @@
+#include "Timer.h"
+
+Timer::Timer()
+{
+    isRunning = false;
+}
+
+Timer::~Timer()
+{
+
+}
+
+
+void Timer::start()
+{
+    gettimeofday(&startData, NULL);
+    isRunning = true;
+    validTime = false;
+}
+
+void Timer::stop()
+{
+    if (isRunning)
+    {
+        gettimeofday(&endData, NULL);
+        isRunning = false;
+        validTime = true;
+    }
+
+    else
+    {
+        validTime = false;
+    }
+}
+
+void Timer::getElapsedTime(char *timeStr)
+{
+    int low, high, index = 0;
+    char temp;
+
+    if (validTime)
+    {
+        seconds = endData.tv_sec - startData.tv_sec;
+        microSeconds = endData.tv_usec - startData.tv_usec;
+
+        if (microSeconds < 0)
+        {
+            microSeconds += 1000000;
+            seconds -= 1;
+        }
+
+        while (microSeconds > 0)
+        {
+            timeStr[index] = char(microSeconds % 10 + '0');
+            microSeconds /= 10;
+            index++;
+        }
+
+        while (index < 6)
+        {
+            timeStr[index] = '0';
+            index++;
+        }
+
+        timeStr[index] = RADIX_POINT;
+        index++;
+
+        if (seconds == 0)
+        {
+            timeStr[index] = '0';
+            index++;
+        }
+
+        while (seconds > 0)
+        {
+            timeStr[index] = char(seconds % 10 + '0');
+            seconds /= 10;
+            index++;
+        }
+
+        timeStr[index] = NULL_CHAR;
+
+        low = 0;
+        high = index - 1;
+
+        while (low < high)
+        {
+            temp = timeStr[low];
+            timeStr[low] = timeStr[high];
+            timeStr[high] = temp;
+
+            low++;
+            high--;
+        }
+    }
+
+    else
+    {
+        strcpy(timeStr, "No Data");
+    }
+}
---
+++ PA1/src/main.cpp
@@ -0,0 +1,74 @@
+#include <mpi.h>
+#include <cstdlib>
+#include <time.h>
+#include <iostream>
+#include "Timer.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int numtasks, rank, dest, source, rc, count, tag = 1;
+    int inmsg, outmsg = 1;
+    MPI_Status Stat;
+    int index;
+    struct timeval tv1, tv2;
+    SimpleTimer timer;
+    double t1, t2, totalTime = 0;
+    chat time[11];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (numtasks < 2)
+    {
+        printf("ERROR: Must specify two tasks!  Quitting...\n");
+        MPI_Abort(MPI_COMM_WORLD, rc);
+        exit(0);
+    }
+
+    if (rank == 0)
+    {
+        if (numtasks > 2)
+        {
+            printf("Numtasks=%d. Only 2 needed. Ignoring extra tasks...\n", numtasks);
+        }
+
+        dest = 1;
+        source = 1;
+
+        for (index = 0; index < 1000; index++)
+        {
+            t1 = MPI_Wtime();
+            timer.start();
+            rc = MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+            rc = MPI_Recv(&inmsg, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &Stat);
+            timer.stop();
+            t2 = MPI_Wtime();
+            timer.getElapsedTime(time);
+            totalTime += t2 - t1;
+        }
+    }
+
+    else if (rank == 1)
+    {
+        dest = 0;
+        source = 0;
+
+        for (index = 0; index < 1000; index++)
+        {
+            rc = MPI_Recv(&inmsg, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &Stat);
+            rc = MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+        }
+    }
+
+    if (rank == 0)
+    {
+        cout << "WTime: " << totalTime << endl
+             << "Timeval: " << time << endl;
+
+    }
+
+    MPI_Finalize();
+}
