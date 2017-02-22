f2249b161773806ce5df33d7f00cbadb8512e84a
BrianJConway <brianc@nevada.unr.edu>
2017-02-21 18:48:46 -0800

    fixed scripts

--- PA1/One_box.sh
+++ PA1/One_box.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
+#SBATCH -n 2
+#SBATCH --mem=2048MB
+#SBATCH --time=00:30:00
+#SBATCH --mail-user=YOUR_EMAIL@DOMAIN.COM
+#SBATCH --mail-type=ALL
 
-# running
-make oneBox
-# be sure to add more commands from your makefile here
-# for example, you may create a target called run_single and run_double
-# (for running on a double box) that contains some special flags.
-# so you may include echo that says what you're doing, and then do it
-# Ex:)
-# echo "PA1 on single box" && make run_single
-
-# cleaning (optional for build script)
-# make clean
+srun mpi_pingPong
