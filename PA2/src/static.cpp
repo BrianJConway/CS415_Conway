3090c08931791a9f174170e3d061852212a54d1b
BrianJConway <brianc@nevada.unr.edu>
2017-03-13 13:32:25 -0700

    debug

--- PA2/src/static.cpp
+++ PA2/src/static.cpp
@@ -172,7 +172,7 @@ for(rowIndex = 0; rowIndex < IMG_HEIGHT; rowIndex++)
 {
     for(colIndex = 0; colIndex < IMG_WIDTH; colIndex++)
     {
-        cout << colors[rowIndex][colIndex] << ," ";
+        cout << colors[rowIndex][colIndex] << ", ";
     }
     cout << endl;
 }
