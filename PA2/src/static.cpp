49966aa1fc546ee83781aff39be6b6efe3182176
BrianJConway <brianc@nevada.unr.edu>
2017-03-14 01:34:16 -0700

    added new scripts

--- PA2/src/static.cpp
+++ PA2/src/static.cpp
@@ -11,10 +11,10 @@
 
 using namespace std;
 
-const int NUM_MEASUREMENTS = 1;
+const int NUM_MEASUREMENTS = 10;
 
-const float IMG_WIDTH = 1000;
-const float IMG_HEIGHT = 1000;
+const float IMG_WIDTH = 500;
+const float IMG_HEIGHT = 500;
 
 const int INT_WIDTH = IMG_WIDTH;
 const int INT_HEIGHT = IMG_HEIGHT;
