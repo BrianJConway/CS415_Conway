fc69be74dd2cd1eff8ad9886376d716bd66a96e5
BrianJConway <brianc@nevada.unr.edu>
2017-03-25 15:46:50 -0700

    debug

--- PA2/src/static.cpp
+++ PA2/src/static.cpp
@@ -13,8 +13,8 @@ using namespace std;
 
 const int NUM_MEASUREMENTS = 1;
 
-const float IMG_WIDTH = 5000;
-const float IMG_HEIGHT = 5000;
+const float IMG_WIDTH = 37000;
+const float IMG_HEIGHT = 37000;
 
 const int INT_WIDTH = IMG_WIDTH;
 const int INT_HEIGHT = IMG_HEIGHT;
