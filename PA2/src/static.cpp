7493c41a7b33c4857be74b81e5c21d94e52bca0f
BrianJConway <brianc@nevada.unr.edu>
2017-03-25 18:47:00 -0700

    changed size

--- PA2/src/static.cpp
+++ PA2/src/static.cpp
@@ -13,8 +13,8 @@ using namespace std;
 
 const int NUM_MEASUREMENTS = 10;
 
-const float IMG_WIDTH = 40000;
-const float IMG_HEIGHT = 40000;
+const float IMG_WIDTH = 1000;
+const float IMG_HEIGHT = 1000;
 
 const int INT_WIDTH = IMG_WIDTH;
 const int INT_HEIGHT = IMG_HEIGHT;
