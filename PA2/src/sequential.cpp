d84150198b3c996f8c2af8746e54a07b37ac3485
BrianJConway <brianc@nevada.unr.edu>
2017-03-26 10:57:32 -0700

    changed sizes

--- PA2/src/sequential.cpp
+++ PA2/src/sequential.cpp
@@ -11,8 +11,8 @@
 
 using namespace std;
 
-const float IMG_WIDTH = 32000;
-const float IMG_HEIGHT = 32000;
+const float IMG_WIDTH = 36000;
+const float IMG_HEIGHT = 36000;
 
 const int NUM_MEASUREMENTS = 1;
 const int INT_WIDTH = IMG_WIDTH;
