d4ca953f4ced3368957648995c3998fa5bf4328b
BrianJConway <brianc@nevada.unr.edu>
2017-03-27 00:47:24 -0700

    sizes

--- PA2/src/static.cpp
+++ PA2/src/static.cpp
@@ -13,8 +13,8 @@ using namespace std;
 
 const int NUM_MEASUREMENTS = 5;
 
-const float IMG_WIDTH = 40000;
-const float IMG_HEIGHT = 40000;
+const float IMG_WIDTH = 4000;
+const float IMG_HEIGHT = 4000;
 
 const int INT_WIDTH = IMG_WIDTH;
 const int INT_HEIGHT = IMG_HEIGHT;
