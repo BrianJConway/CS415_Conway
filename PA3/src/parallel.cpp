9840accf023d9649ecd7c3bab199b187e7e1e865
BrianJConway <brianc@nevada.unr.edu>
2017-04-10 22:42:07 -0700

    debug

--- PA3/src/parallel.cpp
+++ PA3/src/parallel.cpp
@@ -105,7 +105,7 @@ int main(int argc, char *argv[])
             // Sort master's region into small buckets
             region.resize(lastRegSize);
 
-cout << "MASTER STARTED SORTING INTO BUCKETS" << endl;
+//cout << "MASTER STARTED SORTING INTO BUCKETS" << endl;
             for(index = 0; index < lastRegSize; index++, dataIndex++)
             {
                 bucketNum = (float) data[dataIndex] / (( (float) MAX_NUM + 1) / (float) numTasks);
@@ -139,10 +139,10 @@ cout << "MASTER STARTED SORTING INTO BUCKETS" << endl;
             }
             MPI_Barrier(MPI_COMM_WORLD);
 
-            cout << "MASTER STARTING REGION SORT" << endl;
+//cout << "MASTER STARTING REGION SORT" << endl;
             // Sort final bucket
             bubbleSort(region);
-            cout << "MASTER FINISHED REGION SORT" << endl;
+//cout << "MASTER FINISHED REGION SORT" << endl;
 
             // Barrier
             MPI_Barrier(MPI_COMM_WORLD);
@@ -216,7 +216,7 @@ cout << "MASTER STARTED SORTING INTO BUCKETS" << endl;
                         oneBucket.resize(bucketSize);
                         MPI_Recv(&(oneBucket[0]), bucketSize, MPI_INT, srcProcess, tag, MPI_COMM_WORLD, &status);
 
-cout << "Process: " << rank << " got bucket from " << srcProcess << " of size " << bucketSize << endl;
+//cout << "Process: " << rank << " got bucket from " << srcProcess << " of size " << bucketSize << endl;
 
                         // Copy contents to region (big bucket)
                         for(dataIndex = 0; dataIndex < bucketSize; dataIndex++)
@@ -243,10 +243,13 @@ cout << "Process: " << rank << " got bucket from " << srcProcess << " of size " 
 
             MPI_Barrier(MPI_COMM_WORLD);
 
-            cout << "TASK " << rank << " STARTING REGION SORT" << endl;
+//cout << "TASK " << rank << " STARTING REGION SORT" << endl;
             // Sort final bucket
             bubbleSort(region);
-            cout << "TASK " << rank << " FINISHED REGION SORT" << endl;
+//cout << "TASK " << rank << " FINISHED REGION SORT" << endl;
+
+            cout << "TASK " << rank << " HAS " << region.size() << " ITEMS." << endl;
+
             // Barrier
             MPI_Barrier(MPI_COMM_WORLD);
 
