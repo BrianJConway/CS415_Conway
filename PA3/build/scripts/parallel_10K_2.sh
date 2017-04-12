1c78e3d60190f428d663a9db6f41f66c2887242b
BrianJConway <brianc@nevada.unr.edu>
2017-04-11 23:52:34 -0700

    added scripts

---
+++ PA3/build/scripts/parallel_10K_2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -n 2
+#SBATCH -N 1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8192MB
+#SBATCH --time=00:05:30
+#SBATCH --mail-user=brianc@nevada.unr.edu
+#SBATCH --mail-type=ALL
+srun mpi_parallel 10000 y
---
+++ PA3/build/scripts/test/parallel_1K_12.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -n 12
+#SBATCH -N 2
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8192MB
+#SBATCH --time=00:05:30
+#SBATCH --mail-user=brianc@nevada.unr.edu
+#SBATCH --mail-type=ALL
+
+srun mpi_parallel 1000 y
