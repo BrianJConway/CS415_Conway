2172402236a87146f322ec6e7d8f9ec079569e73
BrianJConway <brianc@nevada.unr.edu>
2017-04-12 08:25:10 -0700

    moved scripts

---
+++ PA3/build/scripts/parallel_1M_8.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -n 8
+#SBATCH -N 1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8192MB
+#SBATCH --time=00:05:30
+#SBATCH --mail-user=brianc@nevada.unr.edu
+#SBATCH --mail-type=ALL
+
+srun mpi_parallel 1000000
