e2964351a3b3c04c23365ed35e90230fe8d0d50e
BrianJConway <brianc@nevada.unr.edu>
2017-04-16 17:19:46 -0700

    PA4 test resizing

--- PA4/src/parallel.cpp
+++ PA4/src/parallel.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <stdio.h>
 #include "Timer.h"
+#include <string.h>
 #include <random>
 #include <cmath>
 
@@ -15,13 +16,14 @@ const int NUM_MEASUREMENTS = 1;
 const int MIN_NUM = 0;
 const int MAX_NUM = 999999;
 
-bool outputSorted = false;
-
 void generateNumbers(int matrixSize, vector<vector<int>> &A,
-                     vector<vector<int>> &B, vector<vector<int>> &C);
+                                vector<vector<int>> &B, vector<vector<int>> &C);
+
+void sendChunksFromMaster(int matrixSize, int offset, int numTasks, MPI_Comm comm,
+                                  vector<vector<int>> A, vector<vector<int>> B);
 
 void matrixMult(int matrixSize, vector<vector<int>> A,
-                vector<vector<int>> B, vector<vector<int>> &C);
+                                 vector<vector<int>> B, vector<vector<int>> &C);
 
 void calcStatistics(vector<double> measurements, double &avg, double &stdDev);
 
@@ -29,11 +31,12 @@ int main(int argc, char *argv[])
 {
     // Initialization
     int numTasks, rank, dest, src, tag = 1;
-    int index, matrixSize = 0;
-    vector<vector<int>> A, B, C;
+    int index, rowIndex, colIndex, procIndex, offset, matrixSize = 0;
+    vector< vector<int> > A, B, C, chunkA, chunkB, chunkC; 
     double average, stdDev;
     Timer timer;
     vector<double> timings;
+    bool outputMatrices = false;
     int periods[2] = {1,1};
     int dimensions[2] = {0,0};
     int coords[2] = {-1,-1};
@@ -48,29 +51,44 @@ int main(int argc, char *argv[])
     MPI_Cart_create(MPI_COMM_WORLD, 2, dimensions, periods, 0, &cartComm); 
     MPI_Cart_coords(cartComm, rank, 2, coords);
 
-    // Check if number of elements specified
-    if (argc >= 2)
+    // Make sure number of tasks is a perfect square
+    while(fmod(sqrt((double) numTasks), 1.0) != 0)
     {
-        for(int index = 0; index < numTasks; index++)
-        {
-            if(rank == index)
-            {
-                cout << "Rank: " << rank 
-                     << " at coords " << coords[0] << " " << coords[1] 
-                     << endl;
-            }
-            MPI_Barrier(cartComm);
-        }
-    
+        numTasks--;
+        cout << "Task count adjusted to " << numTasks << endl;
+    }
 
-/*
+    // Check if size of matrix specified
+    if (argc >= 2 && numTasks > 3)
+    {
         // Get number of items to generate
         matrixSize = atoi(argv[1]);
 
-        // Check if argument specified file output
+        // Adjust matrix size if necessary
+        while( (int) pow(matrixSize, 2) % numTasks != 0)
+        {
+            matrixSize--;
+            cout << "Matrix size adjusted to " << matrixSize << "x" << matrixSize <<endl;
+        }
+
+        // Set proper offset
+        offset = matrixSize / sqrt(numTasks);
+/*
+        // Set size of single chunk of matrix
+        chunkA.resize(offset);
+        chunkB.resize(offset);
+        chunkC.resize(offset);
+        for(index = 0; index < offset; index++)
+        {
+            chunkA[index].resize(offset);
+            chunkB[index].resize(offset);
+            chunkC[index].resize(offset, 0);
+        }
+
+        // Check if argument 3 specified file output
         if (argc >= 3 && strcmp(argv[2], "y") == 0)
         {
-            outputSorted = true;
+            outputMatrices = true;
         }
 
         // Check if master
@@ -79,23 +97,58 @@ int main(int argc, char *argv[])
             // Generate specified amount of numbers
             generateNumbers(matrixSize, A, B, C);
 
+            // Send each process their portion
+            sendChunksFromMaster(matrixSize, offset, numTasks, cartComm, A, B);
+
+            // Barrier after chunks sent
+            MPI_Barrier(MPI_COMM_WORLD);
+
+            // Copy own chunks of A and B
+             for(index = 0; index < offset; index++)
+                {
+                    for(colIndex = 0; colIndex < offset; colIndex++)
+                    {
+                        chunkA[index][colIndex] = A[index][colIndex];
+                        chunkA[index][colIndex] = A[index][colIndex];
+                    }
+                }
+
+            // Initialization and multiply once
+
+            // Shift and multiply sqrt(numTasks) times
+            
             // Barrier
             MPI_Barrier(MPI_COMM_WORLD);
+
+            if (outputMatrices)
+            {
+
+            }
+*/
         }
-        else
+        // Check if not master
+        else if(rank < numTasks)
         {
-            // Receive region
-            MPI_Recv(&regionSize, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
-            region.resize(regionSize);
-            MPI_Recv(&(region[0]), regionSize, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+/*
+            // Receive chunks of A and B
+            for(index = 0; index < offset; index++)
+            {
+                // Get current row of chunks for A and B
+                MPI_Recv(&(chunkA[index]), offset, MPI_INT, 0, tag, cartComm, &status);
+                MPI_Recv(&(chunkB[index]), offset, MPI_INT, 0, tag, cartComm, &status);
+            }
+
+            // Barrier after chunks sent
+            MPI_Barrier(MPI_COMM_WORLD);
+
+            // Initialization and multiply once
+
+            // Shift and multiply sqrt(numTasks) times
 
             // Barrier
             MPI_Barrier(MPI_COMM_WORLD);
 
-            // Barrier
-            MPI_Barrier(MPI_COMM_WORLD);
-
-            if (outputSorted)
+            if (outputMatrices)
             {
 
             }
@@ -110,7 +163,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void generateNumbers(int matrixSize, vector<vector<int>> &A,
+void generateNumbers(int matrixSize, vector<vector<int>> &A, 
                      vector<vector<int>> &B, vector<vector<int>> &C)
 {
     // Initialize function/variables
@@ -151,6 +204,37 @@ void generateNumbers(int matrixSize, vector<vector<int>> &A,
         }
     }
     // end loop
+}
+
+void sendChunksFromMaster(int matrixSize, int offset, int numTasks, MPI_Comm comm, 
+                                   vector<vector<int>> A, vector<vector<int>> B)
+{
+    // Initialization
+    int procIndex, rowIndex, colIndex, index, tag = 1;
+
+    // Send each process their chunks
+    procIndex = 1;
+    for(rowIndex = 0; rowIndex < sqrt(numTasks); rowIndex++)
+    {
+        for(colIndex = 0; colIndex < sqrt(numTasks); colIndex++, procIndex++)
+        {
+            // Make sure master is skipped
+            if(rowIndex != 0 && colIndex != 0)
+            {
+                // Send current process their chunks of A and B
+                for(index = 0; index < offset; index++)
+                {
+                    // Send current row portion of A
+                    MPI_Send(&(A[(rowIndex * offset) + index][colIndex * offset]),
+                        offset, MPI_INT, procIndex, tag, comm);
+
+                    // Send current row portion of B
+                    MPI_Send(&(B[(rowIndex * offset) + index][colIndex * offset]),
+                        offset, MPI_INT, procIndex, tag, comm);
+                }
+            }
+        }
+    }
 }
 
 void matrixMult(int matrixSize, vector<vector<int>> A,
