d499ef064327376468b0eed232b828d1bb005788
BrianJConway <brianc@nevada.unr.edu>
2017-04-16 18:58:37 -0700

    master chunk copy tested, removed debug

--- PA4/src/parallel.cpp
+++ PA4/src/parallel.cpp
@@ -109,32 +109,14 @@ int main(int argc, char *argv[])
             MPI_Barrier(cartComm);
 
             // Copy own chunks of A and B
-
-            cout << "Main A master chunk: " << endl;
-
-             for(index = 0; index < offset; index++)
-                {
-                    for(colIndex = 0; colIndex < offset; colIndex++)
-                    {
-                        cout << A[index][colIndex] << " ";
-                    }
-                    cout << endl;
-                }
-                cout << endl << endl;
-
-             cout << "Chunk A in master: " << endl;
-
              for(index = 0; index < offset; index++)
                 {
                     for(colIndex = 0; colIndex < offset; colIndex++)
                     {
                         chunkA[index][colIndex] = A[index][colIndex];
-                        cout << chunkA[index][colIndex] << " ";
                         chunkB[index][colIndex] = B[index][colIndex];
                     }
-                    cout << endl;
                 }
-                cout << endl << endl;
 
 /*
 
