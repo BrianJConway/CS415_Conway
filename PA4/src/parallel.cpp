33eaa6fc25ca48c4a54fa2f016c055527227262a
BrianJConway <brianc@nevada.unr.edu>
2017-04-16 23:05:32 -0700

    debug

--- PA4/src/parallel.cpp
+++ PA4/src/parallel.cpp
@@ -122,8 +122,6 @@ int main(int argc, char *argv[])
         if (coords[1] != 0)
         {
             MPI_Cart_shift(cartComm, 0, -coords[1], &src, &dest);
-            cout << "Rank: " << rank << " at [" << coords[0] << "," << coords[1] << "] sends B to rank " << dest << endl;
-
             for (index = 0; index < offset; index++)
             {
                 MPI_Sendrecv_replace(&(chunkB[index][0]), offset, MPI_INT, dest,
@@ -143,7 +141,7 @@ int main(int argc, char *argv[])
             // Shift A rows once
             MPI_Cart_shift(cartComm, 1, -1, &src, &dest);
             cout << "Rank: " << rank << " at [" << coords[0] << "," << coords[1] << "] sends A to rank " << dest << endl;
-            
+
             for (index = 0; index < offset; index++)
             {
                 MPI_Sendrecv_replace(&(chunkA[index][0]), offset, MPI_INT, dest,
